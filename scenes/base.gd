1e8f920a02e52d11d8f5f8cfecebe78865c77124
Henrique Alberto de Andrade <henrique.alberto.andrade@hotmail.com>
2024-12-28 11:10:51 -0300

    Day 1.5

--- scenes/base.gd
+++ scenes/base.gd
@@ -5,18 +5,44 @@ extends Node2D
 @onready var villagers: Node = $villagers
 @onready var tmr_spawn: Timer = $tmrSpawn
 
+var resourcesCollected:int = 300
+
+var exploreVectors:Array[Vector2] = [Vector2(1, 0), Vector2(0, 1), Vector2(-1, 0), Vector2(0, -1)]
+var resourceLocations:Array[Area2D] = []
+var exploreIndex:int = 0
+
 const CHARACTER = preload("res://scenes/character.tscn")
 
 func _ready() -> void:
 	startSpawnTimer()
 
 func startSpawnTimer():
-	if villagers.get_child_count() < maxSupply:
+	if villagers.get_child_count() < maxSupply && resourcesCollected >= 100:
 		tmr_spawn.start(spawnTimer)
-
+		resourcesCollected -= 100
 
 func _on_tmr_spawn_timeout() -> void:
 	var villager = CHARACTER.instantiate()
 	villagers.add_child(villager)
+	#villager.exploreDirection = exploreVectors[getExploreIndex()]
 	villager.global_position = global_position
+	villager.startRetreat()
 	startSpawnTimer()
+
+func getExploreIndex():
+	exploreIndex = exploreIndex + 1 if exploreVectors.size() - 1 > exploreIndex else 0
+	return exploreIndex
+
+func addResourcePosition(a:Area2D):
+	if !resourceLocations.has(a):
+		resourceLocations.push_front(a)
+
+func getNextExploreDirection():#resourceLocations[0] if resourceLocations.size() > 0 else 
+	return exploreVectors[getExploreIndex()]
+
+func getNextResource():
+	return resourceLocations[0] if resourceLocations.size() > 0 else null
+
+func retrieveResources(v = 1):
+	resourcesCollected += v
+	if villagers.get_child_count() < maxSupply && resourcesCollected >= 100: tmr_spawn.start(spawnTimer)
--- scenes/character.gd
+++ scenes/character.gd
@@ -3,23 +3,60 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 var resource:Area2D = null
+@onready var nav: NavigationAgent2D = $nav
+@onready var tmr_retreat: Timer = $tmrRetreat
 
+var retreating:bool = false
+var collecting:bool = false
 var resourceCollected:int = 0
 @export var maxResource:int = 25
+@export var exploreDirection:Vector2 = Vector2(1,0)
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Vector2(randi_range(-1, 1), randi_range(-1, 1))
-	if direction && (resourceCollected < maxResource || resource == null):
+	#var direction = Vector2(randi_range(-1, 1), randi_range(-1, 1))
+	var direction = exploreDirection
+	if resource != null && resourceCollected < maxResource:
+		nav.target_position = resource.global_position
+		var targetPosition = nav.get_next_path_position()
+		direction = global_position.direction_to(targetPosition) if !nav.is_target_reached() else Vector2(0,0)
+	elif resourceCollected >= maxResource || retreating:
+		nav.target_position = get_parent().get_parent().global_position
+		var targetPosition = nav.get_next_path_position()
+		direction = global_position.direction_to(targetPosition) if !nav.is_target_reached() else Vector2(0,0)
+	if direction: # && (resourceCollected < maxResource || resource == null)
 		velocity = direction * SPEED
 	else:
 		velocity = Vector2(move_toward(velocity.x, 0, SPEED), move_toward(velocity.y, 0, SPEED))
-		if resource:
+		if resource && collecting:
 			resourceCollected += 1
 
 	move_and_slide()
 
+func startRetreat(v = randf_range(5, 25)):
+	tmr_retreat.start(v)
+
 func _on_detection_area_entered(area: Area2D) -> void:
 	if area.is_in_group("resource"):
 		resource = area
+func _on_tmr_retreat_timeout() -> void:
+	if resource == null: retreating = true
+
+func _on_retreat_area_entered(area: Area2D) -> void:
+	if area.is_in_group("resource"):
+		resource = area
+		collecting = true
+	if area.is_in_group("base"):
+		var base = area.get_parent()
+		if resource != null: base.addResourcePosition(resource)
+		base.retrieveResources(resourceCollected)
+		resourceCollected = 0
+		retreating = false
+		exploreDirection = base.getNextExploreDirection()
+		resource = base.getNextResource()
+		startRetreat()
+
+func _on_retreat_area_exited(area: Area2D) -> void:
+	if area.is_in_group("resource"):
+		collecting = false
